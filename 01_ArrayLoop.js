d562dbccbef0bdcbab8c5f816dbeabd384fb41c9
Mursheed <mursheed.khan@qsstechnosoft.com>
2024-07-18 19:15:26 +0530

    high order array

--- 01_ArrayLoop.js
+++ 01_ArrayLoop.js
@@ -1,7 +1,75 @@
-// sum of two numbers
 
-const arr = [1,2,3,4,5]
+const programming = ["java", "python", "ruby"]
 
-for (const num of arr) {
-    console.log(num);
+console.log("Output Using forof in array");
+for (const key of programming) {
+    
+    console.log(key);
 } 
+console.log("Output Using forin in array");
+for (const index in programming) {
+    
+    console.log(index + ":" + programming[index]);
+}
+
+const map = new Map();
+map.set("IN","India");
+map.set("USA","America")
+
+console.log("Output Using forof in map");
+for (const key of map) {    
+    console.log(key);
+}
+
+console.log("Output Using forof in map");
+// Destructure way as below
+for (const [key,value] of map) {
+    
+    console.log(key + ":" + value); // IN:India , USA:America
+}
+console.log("Output Using forin in map");
+for (const [key,value] in map) {    
+    console.log(key + ":" + value);
+}
+
+const myObject = {
+    js:"JAVASCRIPT",
+    java:"JAVA"
+}
+//for (const [key,value] of myObject) {
+    //console.log(key + ":" + value); // myObject is not iterable
+//}
+console.log("Output Using forin in literal object");
+for (const key in myObject) {
+    
+    console.log(key + ":" + myObject[key]);
+}
+
+
+// The most common loop method is foreach and map
+const coding = ["js", "cpp", "php"]
+coding.forEach((item)=>{
+    console.log(item);
+});
+function printMe(item){
+    console.log(item);
+}
+coding.forEach(printMe)
+
+const myCoding = [
+    {
+        languageName:"javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    }
+]
+
+myCoding.forEach((item)=>{
+    let {languageName,languageFileName} = item
+    console.log(languageName + ":" + languageFileName);
+})
+
+
